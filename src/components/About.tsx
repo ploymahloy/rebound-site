7a9eb074aa1ade2ce89d59e8825522b2190ebd14
Patrick Mahloy <patrick.mahloy@gmail.com>
2025-06-30 00:09:31 -0400

    added sam's pic - about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -29,7 +29,7 @@ const About = () => {
           </div>
           <div className="about-image">
             <img
-              src="https://images.unsplash.com/photo-1542744173-8e7e53415bb0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80"
+              src="./public/aboutsam.jpg"
               alt="Industrial facility"
             />
           </div>
